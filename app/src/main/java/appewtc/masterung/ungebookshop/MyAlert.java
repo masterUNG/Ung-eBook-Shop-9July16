ea0bc361072bba570a7ecb108cd013aefb48da43
masterUNG <phrombutr@gmail.com>
2016-07-09 14:13:41 +0700

    SignUp Successful

---
+++ app/src/main/java/appewtc/masterung/ungebookshop/MyAlert.java
@@ -0,0 +1,30 @@
+package appewtc.masterung.ungebookshop;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by masterUNG on 7/9/2016 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context); // Create Constance or object
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/ungebookshop/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/ungebookshop/SignUpActivity.java
@@ -1,16 +1,104 @@
 package appewtc.masterung.ungebookshop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit  นี่คือการประกาศตัวแปรเว้ยเห้ย
+    private EditText nameEditText, surnameEditText, userEditText,
+            passwordEditText;
+    private String nameString, surnameString,
+            userString, passwordString;
+    private static final String urlPHP = "http://swiftcodingthai.com/9july/add_user_master.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind or Initial Widget คือการผูกตัวแปร กับ Widget บน Activity
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
     }   // Main Method
 
+    public void clickSignUpSign(View view) {
+
+        //Get Value from Edit Text
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (checkSpace()) {
+            //True Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องด้วย คะ");
+
+        } else {
+            //False No Space
+
+            updateNewUserToServer();
+
+        } // if
+
+    }   // clickSign
+
+    private void updateNewUserToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+
+
+    }   // update
+
+    private boolean checkSpace() {
+
+        boolean status = false;
+
+        status = nameString.equals("") || surnameString.equals("") ||
+                userString.equals("") || passwordString.equals("");
+
+        return status;
+    }
 
 
 }   // Main Class
